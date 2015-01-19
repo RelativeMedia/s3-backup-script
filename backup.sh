12f0b076b7ec9ec25e8bf18228fdae7dc1fb33e3
Mike H. DeVita <mike@devita.co>
2015-01-19 13:27:12 -0700

    added attachment ability to backups

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source backup.cfg
 sendMail(){
-    msg='{ "async": false, "key": "'$MANDRILLKEY'", "message": { "from_email": "'$FROMEMAIL'", "from_name": "'$FROMNAME'", "return_path_domain": null, "subject": "['$HOSTNAME'] '$1'", "text": "'$2'", "to": [{ "email": "'$SENDTOEMAIL'", "type": "to" }] }}';
+	msg='{ "async": false, "key": "'$MANDRILLKEY'", "message": { "from_email": "'$FROMEMAIL'", "from_name": "'$FROMNAME'", "return_path_domain": null, "subject": "['$HOSTNAME'] '$1'", "text": "'$2'", "attachments": [{ "type": "text/plain", "name": "backup_report.txt", "content": "'$(base64 $2)'" }], "to": [{ "email": "'$SENDTOEMAIL'", "type": "to" }] }}';
     results=$(curl -A 'Mandrill-Curl/1.0' -d "$msg" 'https://mandrillapp.com/api/1.0/messages/send.json' -s 2>&1);
     echo "$results" | grep "sent" -q;
     if [ $? -ne 0 ]; then
@@ -67,6 +67,6 @@ rm -rf $TmpBackupDir/*
 echo 'All Done! Yay! (",)'
 
 ## Email Report of What Exists on S3 in Today's Folder
-s3cmd ls -r --config=$S3ConfigFile $S3URI > "/tmp/s3report.txt"
-EMAILMESSAGE="/tmp/s3report.txt"
+s3cmd ls -r --config=$S3ConfigFile $S3URI > "logs/s3report.txt"
+EMAILMESSAGE="logs/s3report.txt"
 sendMail "Backup Finished" "$EMAILMESSAGE"
